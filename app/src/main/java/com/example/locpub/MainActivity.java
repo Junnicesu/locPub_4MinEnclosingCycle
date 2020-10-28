80a234648481db0256386f5734ff111ee868c94d
Jacob_in_IDE <2018008452@student.sit.ac.nz>
2020-10-29 11:03:45 +1300

    sj: located successfully in simulator

--- app/src/main/java/com/example/locpub/MainActivity.java
+++ app/src/main/java/com/example/locpub/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.locpub;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -27,60 +37,115 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
-
 import static androidx.constraintlayout.motion.widget.Debug.getLocation;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
     TextView textSummary;
-    Button buttonSave;
+    Button buttonSave, buttonLoc;
     EditText editTextDescription;
     Location lastlocation = null;
     FusedLocationProviderClient mFusedLocationClient;
+    private static final int LOCATION_PERMISSION_CODE = 100;
     String posturl = "http://developer.kensnz.com/api/addlocdata";
+
+    boolean prefAutoUpload = false;
+    int prefAutoUploadInterval = 5;
+    String prefFullname = "";
+    String prefUserid = "";
+    String prefLocDescription = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Main");
         buttonSave = findViewById(R.id.buttonSave);
+        buttonLoc = findViewById(R.id.buttonLoc);
         textSummary = findViewById(R.id.textSummary);
         editTextDescription = findViewById(R.id.editTextDescription);
-        mFusedLocationClient =
-                LocationServices.getFusedLocationProviderClient(this);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        prefAutoUpload = prefs.getBoolean("auto_loc_record", false);
+        String strInterval = prefs.getString("upload_interval", "5");
+        strInterval = strInterval.substring(0, strInterval.indexOf(" "));
+        Toast.makeText(this, "strInterval is " + strInterval, Toast.LENGTH_LONG).show();
+        prefAutoUploadInterval = Integer.parseInt(strInterval);
+        prefFullname = prefs.getString("full_name", "");
+        prefUserid = prefs.getString("userid", "");
+        prefLocDescription = prefs.getString("default_description", "");
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         getLocation();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                startActivity(new Intent(this, Settings.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     private void getLocation() {
         buttonSave.setEnabled(false);
-        textSummary.setText("In getting location...");
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @SuppressLint("DefaultLocale")
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-                            lastlocation = location;
-                            buttonSave.setEnabled(true);
-                            textSummary.setText(String.format("(%f,%f)",
-                                    location.getLatitude(),
-                                    location.getLongitude()));
+        textSummary.setText("In permission checking...");
+
+        boolean isFineLocAllowed = ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        boolean isCoarseLocAllowed = ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        if(isFineLocAllowed || isCoarseLocAllowed) {
+            textSummary.setText("Locating...");
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            textSummary.setText("On Success located...");
+                            if (location != null) {
+                                lastlocation = location;
+                                buttonSave.setEnabled(true);
+                                textSummary.setText(String.format("(%f,%f)",
+                                        location.getLatitude(),
+                                        location.getLongitude()));
+                            }
+                            else {
+                                textSummary.setText("location value is null!!");
+                            }
                         }
-                    }
-                });
+                    });
+        }
+        else {
+            textSummary.setText("No permission for location");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                textSummary.setText("request Permission");
+                requestPermissions(new String[]{ ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
+            }
+        }
+    }
+
+
+    public void onClickButtonLocate(View view) {
+        getLocation();
     }
 
     public void onClickButtonSave(View view) {
-        getLocation();
+        if(prefUserid.isEmpty() || prefFullname.isEmpty()){
+            Toast.makeText(this, "Please specify your userid and full name", Toast.LENGTH_LONG).show();
+            return;
+        }
         sendDataToWeb();
     }
 
@@ -124,4 +189,6 @@ public class MainActivity extends AppCompatActivity {
             textSummary.setText(eS.getMessage());
         }
     }
+
+
 }
